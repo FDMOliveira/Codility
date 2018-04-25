f2e920935260dac840c4331d27ed7d8e69c67434
Filipe Oliveira <filipe.dmo@gmail.com>
2018-04-25 13:35:18 +0100

    Update descriptions

--- Lesson 02/CyclicRotation.js
+++ Lesson 02/CyclicRotation.js
@@ -1,7 +1,20 @@
-//  
-//  Elementos do array movem-se X posições para a direita
-//  Quando chegar ao fim do array, voltam ao incio
-//
+/*An array A consisting of N integers is given. 
+Rotation of the array means that each element is shifted right by one index, 
+and the last element of the array is moved to the first place. 
+For example, the rotation of array A = [3, 8, 9, 7, 6] is [6, 3, 8, 9, 7] 
+(elements are shifted right by one index and 6 is moved to the first place).
+
+The goal is to rotate array A K times; that is, each element of A will be shifted
+to the right K times.
+
+For example, given
+    A = [3, 8, 9, 7, 6]
+    K = 3
+the function should return [9, 7, 6, 3, 8]. Three rotations were made:
+    [3, 8, 9, 7, 6] -> [6, 3, 8, 9, 7]
+    [6, 3, 8, 9, 7] -> [7, 6, 3, 8, 9]
+    [7, 6, 3, 8, 9] -> [9, 7, 6, 3, 8]
+ */
 var a=[1,2,3,4,5,6,7,8];
 var x=3;
 var ultima;
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-Codility Exercises Solved in Javascript
+# Codility Exercises Solved in Javascript
+In These repository, there are a few programming logic exercises available by [Codility](https://app.codility.com/programmers/) Website.
+
+1. FizzBuzzWoof
+2. Lesson 2
+    * CyclicRotation
+    * OddOccurrencesInArray
+3. Lesson 3
+    * FrogJmp
+    * PermMissingElem
+3. Lesson 4
+    * FrogRiverOne
+    * MissingInteger
+    * PermCheck    
